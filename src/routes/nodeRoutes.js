ec736c0771c05f5f83558af86f37dba5a8448533
Vtewari2311 <vtewari@uwm.edu>
2023-04-22 17:53:46 -0500

    additionals routes and configs

--- src/routes/nodeRoutes.js
+++ src/routes/nodeRoutes.js
@@ -1,9 +1,24 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const nodeController = require("../controllers/nodeController");
+// Import controller functions
+const {
+  getIndex,
+  getNodeStatus,
+  getAddressTransactions
+} = require('../controllers/nodeController');
 
-// Define routes
-router.get("/", nodeController.getIndex);
+// Route for displaying a list of all available nodes
+router.get('/', getIndex);
+
+// Route for displaying the status of a specific node
+// Example URL: /nodes/127.0.0.1/8333/status
+router.get('/:ip/:port/status', getNodeStatus);
+
+// Route for displaying transactions for a specific address on a specific node
+// Example URL: /nodes/127.0.0.1/8333/transactions/1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa
+router.get('/:ip/:port/transactions/:address', getAddressTransactions);
 
 module.exports = router;
+
+
