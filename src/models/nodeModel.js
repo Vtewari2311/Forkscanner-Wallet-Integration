739ff73b29b6b2b8c6442ca55868f8af7aca4828
Vtewari2311 <vtewari@uwm.edu>
2023-04-22 17:53:37 -0500

    edits

--- src/models/nodeModel.js
+++ src/models/nodeModel.js
@@ -1,21 +1,41 @@
+const { forkscanner } = require("../config/forkscanner");
+
 class NodeModel {
-    constructor(ip, port) {
-      this.ip = ip;
-      this.port = port;
-    }
-  
-    async getStatus() {
-      // Code to get node status from Forkscanner
+  constructor(ip, port) {
+    this.ip = ip;
+    this.port = port;
+  }
+
+  /**
+   * Get the status of the node.
+   * @returns {Promise<Object>} - The node's status information.
+   */
+  async getStatus() {
+    try {
       const status = await forkscanner.getNodeStatus(this.ip, this.port);
       return status;
-    }
-  
-    async getTransactions(address) {
-      // Code to get transactions for a specific address from Forkscanner
-      const transactions = await forkscanner.getTransactionsByAddress(this.ip, this.port, address);
-      return transactions;
+    } catch (error) {
+      console.error(`Error getting node status for ${this.ip}:${this.port}: ${error}`);
+      throw error;
     }
   }
-  
-  module.exports = NodeModel;
+
+  /**
+   * Get the transactions associated with a given address from the node.
+   * @param {string} address - The address to get transactions for.
+   * @returns {Promise<Array<Object>>} - An array of transaction objects associated with the given address.
+   */
+  async getTransactions(address) {
+    try {
+      const transactions = await forkscanner.getTransactionsByAddress(this.ip, this.port, address);
+      return transactions;
+    } catch (error) {
+      console.error(`Error getting transactions for address ${address} from node ${this.ip}:${this.port}: ${error}`);
+      throw error;
+    }
+  }
+}
+
+module.exports = NodeModel;
+
   
