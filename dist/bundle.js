d1b8395c65e677545f87d01c23f4f86c25acf23a
Vtewari2311 <vtewari@uwm.edu>
2023-04-22 20:39:42 -0500

    bundle for project specification

---
+++ dist/bundle.js
@@ -0,0 +1,7 @@
+// Import necessary modules and files
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './client/components/App';
+
+// Render the main application component
+ReactDOM.render(<App />, document.getElementById('root'));
