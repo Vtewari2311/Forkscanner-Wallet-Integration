0ca60e7062ee4968fed01e1640edecc746ec7c33
Vtewari2311 <vtewari@uwm.edu>
2023-04-22 20:38:45 -0500

    initial header implementation

---
+++ client/components/header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/images/logo.png';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <Link to="/" className="navbar-brand">
+            <img src={Logo} alt="Forkscanner Logo" className="logo" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/services" className="nav-link">
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
